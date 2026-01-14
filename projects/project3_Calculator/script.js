b9b0f529afdb8a2121bea6f56f485e7254a4984e
Enoch Yu <247741827+enochyu-official@users.noreply.github.com>
2026-01-14 15:30:13 +0900

    calculator-js-update

--- projects/project3_Calculator/script.js
+++ projects/project3_Calculator/script.js
@@ -1,11 +1,36 @@
 // Declaring Variables
+let buttonId = document.querySelectorAll("#calculator-input");
 
-
-// Input Function
-
-
-function greet(name) {
-  console.log("Hello " + name);
+// Function
+function select(event) {
+  let selectId = event.target.id;
+  console.log(selectId);
 }
 
-greet("Enoch");
+buttonId.addEventListener("click", select);
+
+/*
+MY THOUGHT PROCESS
+
+1. CASE BY CASE
+let select1 = document.getElementById('1');
+let content1 = select1.textContent;
+function display1() {
+  document.getElementById("output").textContent = content1;
+}
+select1.addEventLIstener("click", display1);
+
+2. ACCUMULATION
+select.addEventListener("click", select(event) {
+    let selectId = event.target.id;
+    console.log(selectId);
+  }
+);
+
+function displayHSTR {
+  
+}
+function displayTotal() {
+
+}
+*/
